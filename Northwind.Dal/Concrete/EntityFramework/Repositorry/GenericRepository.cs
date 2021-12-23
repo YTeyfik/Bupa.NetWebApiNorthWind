02cfeb5ac74a5b440bbee0c2c980aae749ef506d
YTeyfik <teyo2086@gmail.com>
2021-12-23 14:34:38 +0300

    initial commit

---
+++ Northwind.Dal/Concrete/EntityFramework/Repositorry/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Dal.Abstract;
+using Northwind.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Concrete.EntityFramework.Repositorry
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : EntityBase
+    {
+        #region Variables
+        protected DbContext context;
+        protected DbSet<T> dbSet;
+        #endregion
+
+        #region Constructor
+
+        public GenericRepository(DbContext context)
+        {
+            this.context = context;
+            this.dbSet = this.context.Set<T>();
+            this.context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        #endregion
+
+        #region Methods
+        public T Add(T entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+            dbSet.Add(entity);
+            return entity;
+        }
+
+        public bool Delete(int id)
+        {
+            return Delete(Find(id));
+        }
+
+        public bool Delete(T entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                context.Attach(entity);
+            }
+            return dbSet.Remove(entity)!=null;
+           
+        }
+
+        public T Find(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public List<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return dbSet.Where(expression);
+        }
+
+        public T Update(T entity)
+        {
+            dbSet.Update(entity);
+            return entity;
+        }
+        #endregion
+    }
+
+}
---
+++ Northwind.Dal/Concrete/EntityFramework/Repositorry/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Dal.Abstract;
+using Northwind.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Concrete.EntityFramework.Repositorry
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public IQueryable OrderReport(int orderId)
+        {
+            return dbSet.AsQueryable();
+        }
+    }
+}
